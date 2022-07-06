1b1af5bf62518841fec362e514a4d9d9d312c096
p0x0q <info.allegation@gmail.com>
2022-07-06 13:21:12 +0900

    make waiter container

--- chsmm.py
+++ chsmm.py
@@ -960,7 +960,7 @@ if __name__ == "__main__":
     saved_args, saved_state = None, None
     if len(args.load) > 0:
         # saved_stuff = torch.load(args.load)
-        saved_stuff = torch.load(args.load) 
+        saved_stuff = torch.load(args.load, map_location={'cuda:0':'cpu'}) 
         saved_args, saved_state = saved_stuff["opt"], saved_stuff["state_dict"]
         for k, v in args.__dict__.iteritems():
             if k not in saved_args.__dict__:
