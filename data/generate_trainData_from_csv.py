32181a69c11b73445ecd74e54b078058bd2962c8
p0x0q <info.allegation@gmail.com>
2022-09-21 16:55:32 +0900

    prefix generate data

--- data/generate_trainData_from_csv.py
+++ data/generate_trainData_from_csv.py
@@ -10,19 +10,20 @@ def load_csv(filename):
             dataset.append(row)
     return dataset
 
-src_train = []
 train_tgt_lines = []
+src_train = []
 tempTexts = ""
 for line in load_csv("2022-09-20.csv"):
     tkey, start_template, text, end_template, rawText = line[0], line[1], line[2], line[3], line[5]
     if rawText:
         # print(rawText)
-        train_tgt_lines.append(tempTexts)
+        if tempTexts:
+            src_train.append(tempTexts)
         tempTexts = ""
-        src_train.append(rawText)
+        train_tgt_lines.append(rawText)
     
     if start_template and text and end_template:
-        tempTexts = start_template + " " + text + " " + end_template
+        tempTexts = tempTexts + start_template + " " + text + " " + end_template + " "
     else:
         tempTexts = tempTexts + text
     # __start_name__ 一成というお店 __end_name__ __start_eatType__ よく天ぷら __end_eatType__ __start_action__ 食べます ___end_action__
@@ -30,7 +31,7 @@ for line in load_csv("2022-09-20.csv"):
 def lines_to_file(filename, lines):
     with open(filename, 'w', encoding='utf8') as f:
         for line in lines:
-            f.write(line + '\r\n')
+            f.write(line + '\n')
             
+lines_to_file("train_tgt_lines.txt", train_tgt_lines)
 lines_to_file("src_train.txt", src_train)
-lines_to_file("train_tgt_train.txt", train_tgt_lines)
